1bdb7d9e8d29f4df3d5800024aedff0251d7a970
yucha <yucha@IVY>
2023-05-29 03:26:02 +1200

    newone 22/05/2023

---
+++ src/Hero/Swordsman.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hero;
+
+/**
+ *
+ * @author yucha
+ */
+public class Swordsman extends Hero{
+    public Swordsman(String name) {
+        setAttack(10);
+        setHealth(100);
+        setMaxHealth(100);
+        setName(name);
+        setOccupation("swordsman");
+}
+}
---
+++ src/Hero/Wizard.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hero;
+
+/**
+ *
+ * @author yucha
+ */
+public class Wizard extends Hero{
+    public Wizard(String name) {
+        setAttack(20);
+        setHealth(70);
+        setMaxHealth(70);
+        setName(name);
+        setOccupation("wizard");
+}
+}
